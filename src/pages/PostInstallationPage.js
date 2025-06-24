8486eed52193b0047f9fbaf1079bf7953074cae9
Codehub169 <codehub@169pi.com>
2025-06-24 11:43:56 +0530

    Applied user request: i am getting a white screen for some reason?These are the server logs: Installing Node.js dependencies...
    added 22 packages, removed 13 packages, changed 9 packages, and audited 589 packages in 2s
    
    9

--- src/pages/PostInstallationPage.js
+++ src/pages/PostInstallationPage.js
@@ -1,5 +1,5 @@
-import React from "@react";
-import { Link } from "@react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 import CodeBlock from "../components/ui/CodeBlock";
 
 function PostInstallationPage() {
